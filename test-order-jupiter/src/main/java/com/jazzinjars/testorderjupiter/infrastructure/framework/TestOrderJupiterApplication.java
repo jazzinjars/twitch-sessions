b434d587551c2f39b32e055279336b07dae0c89b
Héctor <hectorardila@gmail.com>
2021-02-17 13:17:14 +0100

    Project scafolding

---
+++ test-order-jupiter/src/main/java/com/jazzinjars/testorderjupiter/infrastructure/framework/TestOrderJupiterApplication.java
@@ -0,0 +1,13 @@
+package com.jazzinjars.testorderjupiter.infrastructure.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOrderJupiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOrderJupiterApplication.class, args);
+	}
+
+}
